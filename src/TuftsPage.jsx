f33aa109a9f4ae700564f9ecacd05dc05ad9adf1
graceek1m <kimgrace390@gmail.com>
2024-11-24 02:32:20 -0500

    forgot to add back button for Tufts page

--- src/TuftsPage.jsx
+++ src/TuftsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Example data for Tufts discounts
 const tuftsDiscountData = [
@@ -11,17 +12,28 @@ const tuftsDiscountData = [
 ];
 
 function TuftsPage() {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div className="grid-container">
-      {tuftsDiscountData.map((item, index) => (
-        <div className="grid-item" key={index}>
-          <div className="icon">
-            <h3>{item.name}</h3>
-            <p>{item.discount}</p>
-            <p>{item.type}</p>
+    <div>
+      <button onClick={handleGoHome} style={{ margin: '20px', padding: '10px' }}>
+        Go Home
+      </button>
+      <div className="grid-container">
+        {tuftsDiscountData.map((item, index) => (
+          <div className="grid-item" key={index}>
+            <div className="icon">
+              <h3>{item.name}</h3>
+              <p>{item.discount}</p>
+              <p>{item.type}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
